a942c71e16f72b781cce7bf5632206b90adef877
septimus82 <slebbes@googlemail.com>
2026-07-02 09:06:42 +0200

    CREATOR-1a add pure door plan builder

--- engine/editor/creator_mode/__init__.py
+++ engine/editor/creator_mode/__init__.py
@@ -2,6 +2,12 @@
 
 from __future__ import annotations
 
+from .creator_door_plan import (
+    CreatorDoorPlan,
+    CreatorDoorPlanOperation,
+    CreatorDoorPlanRequest,
+    build_creator_door_plan,
+)
 from .creator_inspector import CreatorInspectorField, CreatorInspectorModel, build_creator_inspector
 from .creator_mode_controller import CreatorModeController
 from .creator_overlay import CreatorOverlayLine, CreatorOverlayModel, build_creator_overlay_model
@@ -11,10 +17,14 @@ from .creator_terms import classify_entity_snapshot, friendly_engine_term
 __all__ = [
     "CreatorInspectorField",
     "CreatorInspectorModel",
+    "CreatorDoorPlan",
+    "CreatorDoorPlanOperation",
+    "CreatorDoorPlanRequest",
     "CreatorModeController",
     "CreatorModeSnapshot",
     "CreatorOverlayLine",
     "CreatorOverlayModel",
+    "build_creator_door_plan",
     "build_creator_inspector",
     "build_creator_overlay_model",
     "classify_entity_snapshot",
